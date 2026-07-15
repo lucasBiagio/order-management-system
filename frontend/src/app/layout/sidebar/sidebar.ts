e99cd2d81efdfacbcc7e5ed50d523cd76d8089ac
Lucas Biagio <lucas.fruhvald@gmail.com>
2026-07-15 09:30:36 -0300

    feat(frontend): improve application layout and navigation

--- frontend/src/app/layout/sidebar/sidebar.ts
+++ frontend/src/app/layout/sidebar/sidebar.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
+import {
+  RouterLink,
+  RouterLinkActive
+} from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    RouterModule,
+    RouterLink,
+    RouterLinkActive,
+    MatIconModule,
     MatListModule
   ],
   templateUrl: './sidebar.html',
--- frontend/src/app/layout/toolbar/toolbar.ts
+++ frontend/src/app/layout/toolbar/toolbar.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule
+  ],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss'
 })
